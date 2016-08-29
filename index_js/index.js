0aa802b4ef72ea63fb570d88ba22513c7e9518e2
DFFXT <1559379562@qq.com>
2016-08-29 11:35:22 +0800

    更新

--- index_js/index.js
+++ index_js/index.js
@@ -8,10 +8,17 @@ if(window.XMLHttpRequest){
     var xml=new XMLHttpRequest();
 }
 else var xml=new ActiveXobject("Micorsoft.XMLHttp");
-var ajax=function(url){
+var ajax=function(url,variable){
     xml.open("get",url,false);
     xml.send(null);
-    return xml.responseText;
+    var data=xml.responseText;
+    if(data.indexOf('{')<0){
+        data="{}";
+    }
+    if(variable!=null){
+        return "var "+variable+"="+data;
+    }
+    return "var data="+data;
 }
 //------------
 var createDOM=function(parent,DOM,attr){//-----生成dom
@@ -62,22 +69,44 @@ var getClass=function(){//----获取列表
 }
 
 var getText=function(){//----默认页面
-    eval(ajax("data/android.json"));
-    for(var keyName in data){
-        var div=createDOM(text,'div',{"class":"box"});
-        var img=createDOM(div,'img',{"src":"data/img/"+data[keyName]['img']});
-        var div=createDOM(div,'div',{"class":"describe"});
-        for(var i=0;i<data[keyName]['tag'].length;i++){
-            var tag=createDOM(div,'div',{"class":"tag"});
-            tag.innerText=data[keyName]['tag'][i];
+        var div,img,info,subText,sub_div;
+        var img_flag,subText_flag;
+        function New_item(){
+            div=createDOM(text,'div',{"class":"box"});
+            img=createDOM(div,'img',{"alt":"图片不存在或加载失败"});
+            sub_div=createDOM(div,'div',{"class":"describe"});
+            tag=createDOM(sub_div,'div',{"class":"tag"});
+            info=createDOM(sub_div,'div',{"class":"info"});
+            subText=createDOM(sub_div,'div',{"class":"subText"});
+            img_flag=0;subText_flag=0;
         }
-        
-        var info=createDOM(div,'div',{"class":"info"});
-        info.innerText="日期"+data[keyName]['info'];
-        var subText=createDOM(div,'div',{"class":"subText"});
-        subText.innerText=data[keyName]['subText'];
-    }
-    
+        function find_last_data(path){
+            eval(ajax("data/"+path+'.json',"subdata"));
+            if(!("tag" in subdata)) return 0;
+            New_item();
+            tag.innerText=subdata['tag'];
+            info.innerText="日期"+subdata['date'];
+
+            for(var subdata_keyName in subdata){
+                if(subdata_keyName.substr(0,3)=='img'&&img_flag==0){
+                    img.src='data/'+path+'/'+subdata[subdata_keyName];
+                    img_flag=1;
+                }
+                if(subdata_keyName.substr(0,4)=='text'&&subText_flag==0){
+                    subText.innerText=subdata[subdata_keyName];
+                    subText_flag=1;
+                }
+                if(img_flag==1&&subText_flag==1) break;
+            }
+
+        }
+        var ul_in_item=item.getElementsByTagName('ul');
+        for(var i=0; i<ul_in_item.length;i++){
+            var this_url=ul_in_item[i].lastChild.lastChild.href;
+            this_url=this_url.substr(this_url.indexOf('?')+1);
+            find_last_data(this_url);
+
+        }
 }
 
 var new_page=function(data){//---新页面打开
@@ -85,13 +114,15 @@ var new_page=function(data){//---新页面打开
     title.innerHTML=data.title;
     var span=createDOM(text,'span',{"class":"date"});
     span.innerText=data.date;
+    var span=createDOM(span,'span');
+    span.innerText=" ["+data.tag+"]";
     for(var keyName in data){
         if(keyName.substr(0,3)=="img"){
             var img=createDOM(text,'img',{"src":"data/"+data.root+data[keyName]});
         }
         else if(keyName.substr(0,4)=="text"){
             var subText=createDOM(text,'div',{"class":"subText"});
-            subText.innerText=data[keyName];
+            subText.innerHTML=data[keyName];
         }
         else if(keyName.substr(0,4)=="code"){
             var code=createDOM(text,'code');
@@ -111,4 +142,3 @@ else{
     type.href="index_css/describe.css";
     new_page(ajax(addr));
 }
-
