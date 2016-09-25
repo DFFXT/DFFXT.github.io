94599e1f8aee50ffb478e91ed074acf4f9b4e304
DFFXT <1559379562@qq.com>
2016-09-25 17:09:08 +0800

    字体，优化缓存空间，改变模式

--- canvas/css_js/main.js
+++ canvas/css_js/main.js
@@ -10,37 +10,59 @@ var position=[0,0,0,0];//---位置信息
 
 //-------------------------------------------功能绑定区
 canvas_View.addEventListener('touchstart',function(){//---把开始触摸点定为起点
-	if(config.type=='line'){
+	if(config.type=='pen'){
 		drawTable.getStarPosition(2);	
 	}
-	else if(config.type='rect'){
+	else if(config.type=='rect_new'||config.type=='coordinate'){
 		drawTable.getStarPosition(0);
+		rect_box.style.display='block';
+		rect_box.style.border=canvas.lineWidth+'px '+drawTable.colorOpacity(config.color,config.opacity)+' solid';
 	}
 });
-canvas_View.addEventListener('touchmove',function(){//---更新位置信息
+canvas_View.addEventListener('click',function(){//---画点
+	if(config.type!='pen')return;
+	var x=event.pageX;//--click主要是不与划线时的位置混淆
+	var y=event.pageY;
+	if(config.dot[2]==0){//--没有记录时添加一个记录
+		config.dot[0]=x;
+		config.dot[1]=y;
+	}
+	config.dot[2]+=1;
+	drawTable.dot(x,y);//--其实是画圆
+	if(config.dot[2]==2){
+		config.dot[2]=0;//--重置
+		drawTable.matchDot(x,y);//--连线开始
+	}
 	
-	if(config.type=='line'){
+});
+canvas_View.addEventListener('touchmove',function(){//---更新位置信息
+	if(config.type=='pen'){
 		position[0]=position[2];
 		position[1]=position[3];
 		drawTable.getPosition();
-		drawTable.line();
+		drawTable.pen();
 	}
-	else if(config.type=='rect'){
+	else if(config.type=='rect_new'||config.type=='coordinate'){
 		drawTable.getPosition();
-		drawTable.rect();
+		drawTable.rect_new();
+		if(config.type=='coordinate'){
+			rect_box.style.borderTop='none';
+			rect_box.style.borderRight='none';
+		}
 	}
 	event.preventDefault();//---阻止默认事件
-	
 });
 canvas_View.addEventListener('touchend',function(){//----写入结束，保存入栈
-	
-	if(config.type=='rect'){
+	if(config.type=='rect_new'||config.type=='coordinate'){
 		drawTable.getPosition();
+		drawTable.rect_new();
+		if(config.type=='coordinate'){
+			drawTable.coordinate();
+		}
+		else
 		drawTable.rect();
-		config.img_tmp.src=canvas_View.toDataURL();//-写入结束获取缓存图
-		//---开始才获取有可能会有卡顿现象
+		rect_box.style.cssText='display:none;left:0;top:0;width0;height:0;';
 	}
-	
 	drawTable.createImg();
 });
 color_View.addEventListener('touchstart',function(){//----指定坐标，获取颜色
@@ -53,30 +75,17 @@ color_View.addEventListener('touchmove',function(){//---获取移动坐标，获
 	drawTable.getColor();
 	event.preventDefault();
 });
-lineW.addEventListener('mouseover',function(){//---划线粗细显示选项
-		ul_font.style.display='block';
+lineW.addEventListener('click',function(){//---划线粗细显示选项
+	rangeBox.style.display=rangeBox.style.display!='block'?'block':'none';
+	range.value=rangeVal.innerText=canvas.lineWidth;
+	range.max=config.lineWidth[1];//--设置最大值
+	config.rangeType='lineWidth';
 });
-	ul_font.getElementsByTagName('li')[0].addEventListener('click',function(){
-		if(canvas.lineWidth>1){
-			canvas.lineWidth--;
-			ul_font.getElementsByTagName('li')[1].innerText=canvas.lineWidth;
-		}
-	});
-	ul_font.getElementsByTagName('li')[1].addEventListener('click',function(){
-		ul_font.style.display='none';
-	});
-	ul_font.getElementsByTagName('li')[2].addEventListener('click',function(){
-		if(canvas.lineWidth<30){
-			canvas.lineWidth++;
-			ul_font.getElementsByTagName('li')[1].innerText=canvas.lineWidth;
-		}
-	});
 line.addEventListener('click',function(){//---默认模式
-	config.type='line';
+	config.type='pen';
 });
 rect.addEventListener('click',function(){//---矩形模式
-	config.type='rect';
-	config.img_tmp.src=canvas_View.toDataURL();
+	config.type='rect_new';
 });
 clear.addEventListener('click',function(){//---清空
 	drawTable.clear();
@@ -87,26 +96,113 @@ cure.addEventListener('click',function(){//----回复
 download.addEventListener('click',function(){//----下载【pc】
 	drawTable.download();
 });
-opacity.addEventListener('mouseover',function(){
-	ul_opacity.style.display='block';
+opacity.addEventListener('click',function(){
+	rangeBox.style.display=rangeBox.style.display!='block'?'block':'none';
+	range.max=100;//--设置最大值
+	range.value=(rangeVal.innerText=config.opacity)*100;//init
+	config.rangeType='opacity';
 });
-	ul_opacity.getElementsByTagName('li')[0].addEventListener('click',function(){
-		if(config.opacity>=0.1){
-			config.opacity-=0.1;
-			ul_opacity.getElementsByTagName('li')[1].innerText=config.opacity.toFixed(1);
-		}
+
+coordinate.addEventListener('click',function(){
+	ul_coordinate.style.display=ul_coordinate.style.display!='block'?'block':'none';
+	
+});
+	ul_coordinate.getElementsByTagName('li')[0].addEventListener('click',function(){
+		mathFunction.style.display='block';
 	});
-	ul_opacity.getElementsByTagName('li')[1].addEventListener('click',function(){
-		ul_opacity.style.display='none';
-	});
-	ul_opacity.getElementsByTagName('li')[2].addEventListener('click',function(){
-		if(config.opacity<1){
-			config.opacity+=0.1;
-			ul_opacity.getElementsByTagName('li')[1].innerText=config.opacity.toFixed(1);
-		}
+		calculate.addEventListener('click',function(){//--进行计算
+			drawTable.coordinate();
+			drawTable.mathFunction(equation.value);
+			drawTable.createImg();//--可恢复
+
+		});
+		close_equation.addEventListener('click',function(){//--关闭窗口
+			mathFunction.style.display='none';
+		});
+	ul_coordinate.getElementsByTagName('li')[1].addEventListener('click',function(){
+		config.type='coordinate';
 	});
 
+character.addEventListener('click',function(){//--弹出字体写入框
+	if(rect_box.style.display!='block'){//--显示
+		
+		rect_box.innerHTML="<div id='sureButton'></div><input placeholder='text' type='text' style='width:100px'>";
+		rect_box.style.cssText='width:120px;display:block;background:rgba(240,240,240,0.7);padding:30px;left:100px;top:100px;';
+	}
+	else{//--关闭并还原rect_box
+		rect_box.innerHTML='';
+		rect_box.style.cssText='display:none;left:0;top:0;width:0;height:0;';
+		rangeBox.style.display='none';
+		return;
+	}
+	
+	var inputText=rect_box.getElementsByTagName('input')[0];
+	var sureButton=document.getElementById('sureButton');
+	inputText.style.cssText="font-size:"+config.fontSize+"px;width:120px;background:rgba(255,255,255,.3);border:none;";
+	inputText.focus();
+	inputText.addEventListener('click',function(){
+		event.cancelBubble=true;
+	})
+	//--rect_box里面再加入一层元素
+	sureButton.addEventListener('click',function(){//--确定
+		event.cancelBubble=true;
+		var x=rect_box.offsetLeft+30;
+		var y=rect_box.offsetTop+sureButton.clientHeight+30+inputText.clientHeight;
+		drawTable.fillText(inputText.value,x,y);
+	});
+	rect_box.addEventListener('click',function(){//--弹出字体大小选择框
+		rangeBox.style.display=rangeBox.style.display!='block'?'block':'none';
+		rangeVal.innerText=(range.value=config.fontSize)+'px';
+		range.max=config.maxFontSize//--设置最大值
+		config.rangeType='font';
+	});
+	rect_box.addEventListener('touchstart',function(){
+		drawTable.getStarPosition(2);
+	});
+	rect_box.addEventListener('touchmove',function(){event.preventDefault();
+		position[0]=position[2];
+		position[1]=position[3];
+		drawTable.getPosition();
+		drawTable.move(rect_box);
 
+	});
+	rect_box.addEventListener('touchend',function(){
+		drawTable.getStarPosition[2];
+	});
+});
+
+
+
+rangeVal.addEventListener('touchstart',function(){
+	drawTable.getStarPosition(2);
+});
+rangeVal.addEventListener('touchmove',function(){event.preventDefault();
+	position[0]=position[2];
+	position[1]=position[3];
+	drawTable.getPosition();
+	drawTable.move(rangeBox);
+});
+rangeVal.addEventListener('touchend',function(){
+	drawTable.getStarPosition[2];
+});
+range.addEventListener('touchmove',function(){
+	if(config.rangeType=='opacity'){
+		rangeVal.innerText=range.value/100;
+	}else if(config.rangeType=='font'){
+		rangeVal.innerText=rect_box.getElementsByTagName('input')[0].style.fontSize=range.value+'px';
+	}
+	else{
+		rangeVal.innerText=range.value;
+	}
+});
+range.addEventListener('touchend',function(){
+	rangeBox.style.display='none';
+	switch(config.rangeType){
+		case 'lineWidth':canvas.lineWidth=range.value;;break;
+		case 'opacity':config.opacity=range.value/100;break;
+		case 'font':config.fontSize=range.value;break;
+	}
+});
 //-----------------------------------------------------------------
 var operate = function(){//----操作函数集合
 	canvas = canvas_View.getContext('2d');//---init
@@ -120,32 +216,104 @@ var operate = function(){//----操作函数集合
 	color_c.fillStyle=linear;
 	color_c.fillRect(0,0,config.size[0],40);
 
-	this.changeType=function(color,opacity){
+	this.colorOpacity=function(color,opacity){
 		var start=color.lastIndexOf(',');
 		var end=color.lastIndexOf(')');
 		var res=color.substr(0,start+1)+opacity+color.substr(end);
 		return res;
 	}
-	this.line=function(){//----划线
-		canvas.beginPath();
-		canvas.moveTo(position[0],position[1]);
-		canvas.lineTo(position[2],position[3]);
-		canvas.strokeStyle=drawTable.changeType(config.color,config.opacity);
+	this.pen=function(x1,y1,x2,y2){//----划线
+		var X1=x1||position[0],Y1=y1||position[1],X2=x2||position[2],Y2=y2||position[3];
+		canvas.beginPath();//--上行为缺省值
+		canvas.moveTo(X1,Y1);
+		canvas.lineTo(X2,Y2);
+		canvas.strokeStyle=drawTable.colorOpacity(config.color,config.opacity);
 		canvas.closePath();
 		canvas.stroke();
+	}
+	this.rect_new=function(){//--矩形div模拟
+
+		rect_box.style.left=position[0]+'px';
+		rect_box.style.top=position[1]+'px';
+		rect_box.style.width=position[2]-position[0]-1+'px';
+		rect_box.style.height=position[3]-position[1]-1+'px';
 	}
 	this.rect=function(){//----写入矩形
-		canvas_tmp.clearRect(0,0,config.size[0],config.size[1]);//---清空缓存画布，因为填充物为透明背景
-		canvas_tmp.drawImage(config.img_tmp,0,0);//---清空后加入填充物
-		var imgData=canvas_tmp.getImageData(position[0]+1,position[1],position[2]-position[0]-2,position[3]-position[1]-2);
-		//----获取矩形中间的图像
+		if(position[0]==position[2])return;
 		canvas.beginPath();
-		canvas.strokeStyle=config.color;
-		canvas.rect(position[0],position[1],position[2]-position[0],position[3]-position[1]);
+		canvas.strokeStyle=drawTable.colorOpacity(config.color,config.opacity);
+		canvas.rect(position[0]+canvas.lineWidth/2,position[1]+canvas.lineWidth/2,position[2]-position[0]+canvas.lineWidth,position[3]-position[1]+canvas.lineWidth);
+		//---div元素的border和画矩形的边框不是同一个机制
+		canvas.closePath();
+		canvas.stroke();	
+	}
+	this.dot=function(x,y,type){//--点的生成 用于连线
+		canvas.beginPath();
+		var type=type||'stroke';
+		if(type=='fill'){
+			canvas.fillStyle=drawTable.colorOpacity(config.color,config.opacity);
+			canvas.arc(x,y,config.radius,0,Math.PI*2,false);
+		}
+		if(type=='stroke'){
+			canvas.strokeStyle=config.color;
+			canvas.arc(x,y,config.radius,0,Math.PI*2,false);
+			canvas.stroke();
+		}
+		canvas.closePath();
+	}
+	this.matchDot=function(x,y){//--连接两个点
+		canvas.beginPath();
+		canvas.strokeStyle=drawTable.colorOpacity(config.color,config.opacity);
+		canvas.moveTo(config.dot[0],config.dot[1]);
+		canvas.lineTo(x,y);
 		canvas.closePath();
 		canvas.stroke();
-		canvas.putImageData(imgData,position[0]+1,position[1]);//---将缓存的矩形中心图像载入正在画的矩形
 	}
+	this.coordinate=function(){//--建立坐标
+		//---在进行画矩形时，只保留邻边就可以自定义建立起简易坐标
+		drawTable.pen(position[0],position[3],position[0],position[1]);
+		drawTable.pen(position[0],position[3],position[2],position[3]);
+		config.basePoint[0]=position[0];//--保存坐标轴基本点
+		config.basePoint[1]=position[3];
+		config.basePoint[2]=position[0];
+		config.basePoint[3]=position[1];
+		config.basePoint[4]=position[2];
+		config.basePoint[5]=position[3];
+	}
+	this.mathFunction=function(equation){//---数学函数y=2*x
+		var equation=equation||"0.5*x";var y;
+		//--标准转换式 y=k*x --> y=-k*(x-config.basePoint[0])+config.basePoint[1] //--将坐标轴的方向进行转换
+		//--存在转换缺陷，x*x时图形完全不形象，需要缩短x，y轴
+		equation=equation.replace('x','(x-'+config.basePoint[0]+')');//--x值进行替换
+		equation='-1*('+equation+')+'+config.basePoint[1];
+		for(var x=config.basePoint[0];x<config.basePoint[4];x+=0.01){//--0.01为精度
+			eval('y='+equation);//---y=k*x
+			drawTable.pen(x,y,++x,eval('y='+equation));//--传入两个坐标
+		}
+	}
+	this.move=function(obj){
+		var obj_left=rPx(getComputedStyle(obj)['left'],'left');
+		var obj_top=rPx(getComputedStyle(obj)['top'],'top');
+		obj.style.left=obj_left+position[2]-position[0]+'px';
+		obj.style.top=obj_top+position[3]-position[1]+'px';
+	}
+	this.fillText=function(text,x,y,type){//---生成文字
+		var x=x||10,y=y||10,type=type||'fill';
+		canvas.font='normal '+config.fontSize+'px 微软雅黑';
+		if(config.fontSize<16){
+			 var compatibility=0;
+		}else{
+			var compatibility=config.fontSize/3;
+		}
+		if(type='fill'){
+			canvas.fillStyle=drawTable.colorOpacity(config.color,config.opacity);
+			canvas.fillText(text,x,y-compatibility);
+		}else{
+			canvas.strokeText(text,x,y);
+		}
+		drawTable.createImg();
+	}
+
 	this.getColor=function(){//---获取点击处的颜色[rgba模式,a/255为可用a]
 		var offsetT=color_View.offsetTop;
 		var color=color_c.getImageData(position[2],position[3]-config.size[1]-3,1,1).data;
@@ -161,9 +329,6 @@ var operate = function(){//----操作函数集合
 		position[2]=event.changedTouches[0].pageX;
 		position[3]=event.changedTouches[0].pageY;
 	}
-	this.getLastPosition=function(){
-
-	}
 	this.clear=function(val){//---清除最后操作
 		if(val==0){
 			canvas.clearRect(0,0,config.size[0],config.size[1]);
@@ -174,11 +339,11 @@ var operate = function(){//----操作函数集合
 			canvas.clearRect(0,0,config.size[0],config.size[1]);
 			config.pointer=-1;
 			drawTable.createImg();
-			config.img_tmp.src=canvas_View.toDataURL();//---清除后获取新的缓存图
 		}
 	}
 	this.cure=function(){//---回复上一步
 		if(config.pointer>=1){
+			config.dot[2]=0;
 			config.img_tmp = new Image();
 			config.img_tmp.src=config.stage[config.pointer-1];
 			config.img_tmp.onload=function(){//---加载完成再清除不会出现闪烁
@@ -197,7 +362,7 @@ var operate = function(){//----操作函数集合
 				config.stage[i]=config.stage[i+1];
 			}
 		}
-		if(config.pointer<config.cure_len-1){
+		if(config.pointer<config.cure_len-1){//--指针后移
 			config.pointer++;
 		}
 		config.stage[config.pointer]=canvas_View.toDataURL();
@@ -215,4 +380,5 @@ var operate = function(){//----操作函数集合
 	drawTable = new operate();//---初始化功能函数
 	drawTable.createImg();//---第一个图片保存
 	canvas.lineWidth=3;
+	canvas.font='normal '+config.fontSize+'px 微软雅黑';
 }
--- css_js/layout.js
+++ css_js/layout.js
@@ -4,53 +4,72 @@ window.innerHeight
 window.outerHeight
  */
 
-var config={
-	'size':[document.documentElement.clientWidth,document.documentElement.clientHeight*0.8],
+if(document.cookie!=''){
+	document.cookie='width='+document.documentElement.clientWidth;
+	document.cookie='height='+document.documentElement.clientHeight*0.75;
+}
+var config={///---各种配置
+	'size':[document.documentElement.clientWidth,document.documentElement.clientHeight*0.75],
 	'color_heigth':'40px',
-	'opacity':0,
-	'type':'line',//---默认操作
-	'lineWidth':1,//----默认画笔粗细
+	'opacity':1,
+	'type':'pen',//---默认画笔操作
+	'rangeType':'',//--range使用者
+	'lineWidth':[3,20],//----默认画笔粗细/最大画笔粗细
+	'fontSize':16,//--默认字体大小
+	'maxFontSize':100,//--最大字体
+	'radius':2,//---画圆的半径
+	'basePoint':[0,0,0,0,0,0],//---坐标轴基本点
+	'dot':[-1,-1,0],//--连线时的点的保存【最后一个的参数:点数,偶数就进行连接】
 	'color':'rgba(0,0,0,1)',//---默认颜色
 	'stage':[],//----保存操作步骤
 	'pointer':-1,//---当前指针位置
-	'cure_len':10,//---可恢复次数
-	'img_tmp':new Image()//---图片缓存【用于恢复和保存矩形，圆覆盖的中心区域】
+	'cure_len':10,//---可恢复次数【需要一定的缓存空间】
+	'img_tmp':new Image()//---图片缓存【用于恢复】
 };
-(function(){
-
-	function rPx(originData){//---清除单位，并返回int
-		return parseInt(originData.replace('px',''));
+if(document.cookie!=''){
+	var cookies=document.cookie.split(';');
+	for(var i=0;i<cookies.length;i++){
+		eval(cookies[i]);
 	}
+	config.size[0]=width;
+	config.size[1]=height;
+}
+function rPx(originData,type){//---清除单位，并返回int
+	if(type=='top'){
+		var type=document.documentElement.clientHeight;
+	}else{
+		var type=document.documentElement.clientWidth;
+	}
+	if(originData.indexOf('%')>0){
+		return parseInt(originData.replace('%',''))/100*type;
+	}else
+	return parseInt(originData.replace('px',''));
+}
+(function(){
+	function pos(obj,item_amount){//--按键弹出框位置
+		obj.style.width=obj.parentNode.offsetWidth+'px';
+		var styleList=getComputedStyle(obj.parentNode);
+		var ul_parent_padding_bottom=styleList['padding-bottom'];
+		obj.style.marginLeft='-'+(rPx(styleList['padding-left'])+1)+'px';
+		var ul_font_styleList=getComputedStyle(obj.getElementsByTagName('li')[0]);
+		obj.style.marginTop='-'+(rPx(ul_font_styleList['height'])*(item_amount+1)+6)+'px';
+	}
+	function equation_input_layout(obj){
+		obj.style.width=config.size[0]*0.8+'px';
+	}
+	equation_input_layout(mathFunction);
+	pos(ul_coordinate,2);
 	canvas_View.setAttribute('width',config.size[0]+'px');
 	canvas_View.setAttribute('height',config.size[1]+'px');
 	canvas_tmp_View.setAttribute('width',config.size[0]+'px');
 	canvas_tmp_View.setAttribute('height',config.size[1]+'px');
 	color_View.setAttribute('width',config.size[0]+'px');
 	color_View.setAttribute('height',config.color_heigth);
-
-	ul_font.style.width=ul_font.parentNode.offsetWidth+'px';//---设置字体选择的位置
-	ul_opacity.style.width=ul_opacity.parentNode.offsetWidth+'px';//---设置字体选择的位置
-	var styleList=getComputedStyle(ul_font.parentNode);
-
-	var ul_parent_padding_bottom=styleList['padding-bottom'];
-	ul_font.style.marginLeft='-'+(rPx(styleList['padding-left'])+1)+'px';
-	ul_opacity.style.marginLeft='-'+(rPx(styleList['padding-left'])+1)+'px';
-	var ul_font_styleList=getComputedStyle(ul_font.getElementsByTagName('li')[0]);
-	ul_font.style.marginTop='-'+(rPx(ul_font_styleList['height'])*4+6)+'px';
-	ul_opacity.style.marginTop='-'+(rPx(ul_font_styleList['height'])*4+6)+'px';
-	
-	var td=table.getElementsByTagName('td');
-	/*for(var i=0;i<td.length;i++){//---点击变色
-		td[i].addEventListener('click',function(){
-			for(var j=0;j<td.length;j++){
-				td[j].style.background="#fff";
-			}
-			event.target.style.background="gray";
-		});
-	}*/
-	window.onorientationchange=function(){
-		var w=screen.availWidth;
 		
+	var td=table.getElementsByTagName('td');
+
+	window.onorientationchange=function(){
+		var w=screen.availWidth;	
 	}
 })();
 
